4445e63dba2b5b07cec8403d793495bc4621eb91
J'Neil Cottle <jneilcottle@225.25/128.108.219.129.in-addr.arpa>
2018-01-11 16:57:26 -0700

    added legend, temp colors

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-
+matplotlib.rc('xtick', labelsize=8)
+matplotlib.rc('ytick', labelsize=8)
 ##### Runs to have spectra generated #######
 run1 = { 'Name':'T0.3_v1000_chi300_cond',
         'Dir':'../../Blob_paper2/Files/',
@@ -127,19 +129,19 @@ run19 = { 'Name':'HC_v1000_chi300_cond',
         'Mach':3.5,
         'f_list':['0054', '0060', '0080', '0107'],
         'marker':'s',
-        'color':'#ae66bc'}
+        'color':'#4c95cb'}
 run20 = { 'Name':'HC_v1700_chi1000_cond',
         'Dir':'../../Blob_paper3/Files/',
         'Mach':3.5,
         'f_list':['0024', '0050', '0082', '0083'],
         'marker':'s',
-        'color':'#790090'}
+        'color':'#008317'}
 run21 = { 'Name':'HC_v3000_chi3000_cond',
         'Dir':'../../Blob_paper3/Files/',
         'Mach':3.5,
         'f_list':['0007', '0015', '0026', '0049'],
         'marker':'s',
-        'color':'#540064'}
+        'color':'#a87007'}
 run22 = { 'Name':'LowCond_v1700_chi300_cond',
         'Dir':'../../Blob_paper3/Files/',
         'Mach':3.5,
@@ -160,20 +162,20 @@ runList = []
 runList.append(run1)
 runList.append(run2)
 runList.append(run3)
-runList.append(run5)
-runList.append(run4)
 runList.append(run7)
 runList.append(run6)
+runList.append(run5)
+runList.append(run4)
 runList.append(run8)
 runList.append(run9)
 runList.append(run10)
 runList.append(run11)
-runList.append(run13)
-runList.append(run14)
-runList.append(run12)
 runList.append(run17)
 runList.append(run15)
 runList.append(run16)
+runList.append(run13)
+runList.append(run14)
+runList.append(run12)
 runList.append(run18)
 runList.append(run19)
 runList.append(run20)
@@ -185,43 +187,63 @@ runList.append(run22)
 ion1 = {'ion':'O VI',
             'fieldname':'O_p5_number_density',
             'ionfolder': '/OVI/',
-            'color': 'brown'}
+            'color': 'brown',
+            'axis': [2, 1],
+            'loc':[-2.25, 2.5]}
 ion2 = {'ion':'Mg II',
             'fieldname':'Mg_p1_number_density',
             'ionfolder': '/MgII/',
-            'color': 'green'}
+            'color': 'green',
+            'axis': [3, 0],
+            'loc':[-1.75, 0.25]}
 ion3 = {'ion':'N V',
             'fieldname':'N_p4_number_density',
             'ionfolder': '/NV/',
-            'color': 'red'}
+            'color': 'red',
+            'axis': [2, 0],
+            'loc':[-2.25, 2.5]}
 ion4 = {'ion':'C IV',
             'fieldname':'C_p3_number_density',
             'ionfolder': '/CIV/',
-            'color': 'orange'}
+            'color': 'orange',
+            'axis': [1, 0],
+            'loc':[-2.25, 4.5]}
 ion5 = {'ion':'Si III',
             'fieldname':'Si_p2_number_density',
             'ionfolder': '/SiIII/',
-            'color': 'cyan'}
+            'color': 'cyan',
+            'axis': [4, 1],
+            'loc':[3.75, 5.5]}
 ion6 = {'ion':'Si IV',
             'fieldname':'Si_p3_number_density',
             'ionfolder': '/SiIV/',
-            'color': 'blue'}
+            'color': 'blue',
+            'axis': [1, 1],
+            'loc':[-2.25, 4.5]}
 ion7 = {'ion':'Ne VII',
             'fieldname':'Ne_p6_number_density',
             'ionfolder': '/NeVII/',
-            'color':'pink'}
+            'color':'pink',
+            'axis': [3, 1],
+            'loc':[-2.25, 0.25]}
 ion8 = {'ion':'H I',
             'fieldname':'H_p0_number_density',
             'ionfolder': '/HI/',
-            'color':'gray'}
+            'color':'gray',
+            'axis': [0, 0],
+            'loc':[-3.75, 5.5]}
 ion9 = {'ion':'C II',
             'fieldname':'C_p1_number_density',
             'ionfolder': '/CII/',
-            'color':'purple'}
+            'color':'purple',
+            'axis': [4, 0],
+            'loc':[3.75, 5.5]}
 ion10 = {'ion':'C III',
             'fieldname':'C_p2_number_density',
             'ionfolder': '/CIII/',
-            'color': 'magenta'}
+            'color': 'magenta',
+            'axis': [0, 1],
+            'loc':[-2.25, 5.5]}
 
 
 ionList = []
@@ -229,11 +251,11 @@ ionList.append(ion1)
 ionList.append(ion2)
 ionList.append(ion3)
 ionList.append(ion4)
-ionList.append(ion5)
+#ionList.append(ion5)
 ionList.append(ion6)
 ionList.append(ion7)
 ionList.append(ion8)
-ionList.append(ion9)
+#ionList.append(ion9)
 ionList.append(ion10)
 
 def readbestTauB(ion, runName):
@@ -253,38 +275,30 @@ def model(t, b, x):
     return t*(0.01)/(1.01-x**b)
 
 #fig, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
-
+fig, ax = plt.subplots(4, 2, sharey='row')
 for ion in ionList:
-    fig = plt.figure()
-    ax = plt.subplot(111)
+
     for run in runList:
         taus, bs = readbestTauB(ion, run['Name'])
         x = np.linspace(0, 1, 7880)
         #profile = model(taus[0], bs[0], x)
         #ax.plot(x[0:7800], profile[0:7800], label = ion['ion'], color = ion['color'])
-    #for i in range(len(taus)):
-    #    if i==0:
-    #        j=0
-    #        k=0 #top left
-    #    if i==1:
-    #        j=0
-    #        k=1 #top right
-    #    if i==2:
-    #        j=1 #lower left
-    #        k=0
-    #    if i==3:
-    #        j=1 #lower right
-    #        k=1
-        ax.scatter(np.log10(taus), np.log10(bs), label=run['Name'], marker = run['marker'], color = run['color'])
+        ax[ion['axis'][0], ion['axis'][1]].scatter(np.log10(bs), np.log10(taus), label=run['Name'], marker = run['marker'], color = run['color'])
 
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=10)
-    ax.set_title(ion['ion'])
-    ax.set_xlabel('log(tau)')
-    ax.set_ylabel('log(b)')
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    #plt.xscale('log')
-    fig.set_size_inches(12, 6)
-    #fig.savefig('test.png')
-    fig.savefig('../rankTau'+ion['ionfolder']+ion['ionfolder'][1:-1]+'.png')
-    fig.savefig('tauBAnalysis_all/'+ion['ionfolder'][1:-1]+'.png')
+
+    #ax[ion['axis'][0], ion['axis'][1]].set_title(ion['ion'])
+    ax[ion['axis'][0], ion['axis'][1]].annotate(ion['ion'], xy = ion['loc'] )
+
+ax[3,0].set_xlabel('log(b)')
+ax[2,0].set_ylabel('log(tau)')
+ldg = ax[3, 0].legend(loc='upper left', bbox_to_anchor=(0.15, -0.2), fontsize=8, ncol=3)
+#box = ax[ion['axis'][0], ion['axis'][1]].get_position()
+#ax[ion['axis'][0], ion['axis'][1]].set_position([box.x0, box.y0, box.width * 0.8, box.height])
+fig.subplots_adjust(wspace=0.05)
+fig.subplots_adjust(hspace=0.1)
+fig.set_size_inches(8, 10)
+plt.tight_layout()
+
+
+#fig.savefig('../rankTau'+ion['ionfolder']+ion['ionfolder'][1:-1]+'.png')
+fig.savefig('tauBAnalysis_all/jumboTemp.png', bbox_extra_artists=(ldg,), bbox_inches='tight')
